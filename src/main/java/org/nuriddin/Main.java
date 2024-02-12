eae691a8a53238342a37c88bb5eeb678cf220a68
nuriddin5 <nuriddinqurbonboyev@mail.ru>
2024-02-12 18:04:44 +0500

    first

---
+++ src/main/java/org/nuriddin/Main.java
@@ -0,0 +1,17 @@
+package org.nuriddin;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(new AbrorBot());
+        System.out.println("Bot ishladi");
+
+
+    }
+}
